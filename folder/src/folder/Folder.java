5a992007135053a75ea4162b2a6b5097fa658af9
admin <admin@14b>
2018-08-29 11:34:13 +0700

    kampretluuu

---
+++ folder/src/folder/Folder.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package folder;
+
+/**
+ *
+ * @author admin
+ */
+public class Folder {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //aaaaaaaaaaaaa
+    }
+    
+}
